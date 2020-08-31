5836be713a8344dd2b4e9f5588c4759204e03ed8
Oscar Wellner <o.wellner@telfort.nl>
2020-08-31 21:11:09 +0200

    step 4: created main layout

--- app/src/main/java/com/nonamer777/madlevel1task1/HigherLowerActivity.kt
+++ app/src/main/java/com/nonamer777/madlevel1task1/HigherLowerActivity.kt
@@ -2,10 +2,12 @@ package com.nonamer777.madlevel1task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nonamer777.madlevel1task1.databinding.ActivityHigherLowerBinding
 
-class MainActivity : AppCompatActivity() {
+class HigherLowerActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_higher_lower)
     }
 }
